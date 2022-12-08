f1284c39201477ac6eec97a1138924b17240d5ae
DriCro6663 <dri.cro.6663@gmail.com>
2022-12-09 04:31:35 +0900

    new commit

--- bmovt.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'bmovt.exe' with Python DLL being found.
-set PATH=c:\users\G6704~1.YAM\ANACON~1\envs\bmovt;%PATH%
-set PYTHONHOME=C:\Users\G.Yamada\Anaconda3\envs\bmovt
-"%~dp0.\bmovt.exe" %*
